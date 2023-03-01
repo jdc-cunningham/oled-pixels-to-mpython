6ffd533e654e40c182f042f9f3cc7e7178dc349d
Jacob Cunningham <jcun4128@gmail.com>
2023-02-28 18:03:49 -0600

    too slow

---
+++ index.js
@@ -0,0 +1,11 @@
+const pixelRows = 640;
+const pixelCols = 400;
+
+const pixels = document.getElementById('pixels');
+
+for (let r = 0; r < pixelRows; r++) {
+  for (let c = 0; c < pixelCols; c++) {
+    // pixels.innerHTML += '<div class="pixel"></div>';
+    console.log('loop');
+  }
+}
