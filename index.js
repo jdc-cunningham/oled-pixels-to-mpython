114d9af5e57e5ca7f8ca9829be44a35f1f862dc1
Jacob Cunningham <jcun4128@gmail.com>
2023-02-28 18:24:55 -0600

    test

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-const pixelRows = 640;
-const pixelCols = 400;
+// https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Pixel_manipulation_with_canvas
 
 const pixels = document.getElementById('pixels');
 
-for (let r = 0; r < pixelRows; r++) {
-  for (let c = 0; c < pixelCols; c++) {
-    // pixels.innerHTML += '<div class="pixel"></div>';
-    console.log('loop');
-  }
+const img = new Image();
+img.crossOrigin = "anonymous";
+img.src = "./oled-base-img.jpg";
+const canvas = document.getElementById("pixels");
+const ctx = canvas.getContext("2d");
+img.addEventListener("load", () => {
+  ctx.drawImage(img, 0, 0);
+  img.style.display = "none";
+});
+const hoveredColor = document.getElementById("hovered-color");
+const selectedColor = document.getElementById("selected-color");
+
+function pick(event, destination) {
+  const bounding = canvas.getBoundingClientRect();
+  const x = event.clientX - bounding.left;
+  const y = event.clientY - bounding.top;
+  const pixel = ctx.getImageData(x, y, 1, 1);
+  const data = pixel.data;
+
+  const rgba = `rgba(${data[0]}, ${data[1]}, ${data[2]}, ${data[3] / 255})`;
+  destination.style.background = rgba;
+  destination.textContent = rgba;
+
+  return rgba;
 }
+
+canvas.addEventListener("mousemove", (event) => pick(event, hoveredColor));
+canvas.addEventListener("click", (event) => pick(event, selectedColor));
